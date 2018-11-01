16117d1beceab55531b6dc290840114a1964c4d0
Anthony Duzhenko <duha.06@ukr.net>
2018-11-01 18:11:37 +0200

    display numbers in loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,13 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class NumbersActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_numbers);
+
+		List<String> theEngNumbers = Arrays.asList("one", "two", "three", "four",
+					"five", "six", "seven", "eight", "nine", "ten");
+
+		LinearLayout theLayout = (LinearLayout) findViewById(R.id.rootView);
+		for (String theNumber : theEngNumbers) {
+			TextView theView = new TextView(this);
+			theView.setText(theNumber);
+			theLayout.addView(theView);
+		}
 	}
 }
