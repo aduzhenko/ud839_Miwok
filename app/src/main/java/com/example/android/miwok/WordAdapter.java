f9073975cdd342edcdc89dbd4b588fbc497ecb0d
unknown <duha.06@ukr.net>
2018-12-05 10:06:31 +0200

    fix crash with relative layout

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -32,8 +32,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
 						R.layout.word_item, parent, false);
 		}
 
-		LinearLayout theLayout = (LinearLayout) theListItemView
-					.findViewById(R.id.text_layout);
+		RelativeLayout theLayout = theListItemView.findViewById(R.id.text_layout);
 		theLayout.setBackgroundColor(mColor);
 
 		Word theCurrentword = getItem(position);
